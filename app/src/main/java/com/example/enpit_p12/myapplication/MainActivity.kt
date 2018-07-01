9b041320073d7b8e5ba8ded918bcbe7ae6ea652a
uetsh <g1523018@tcu.ac.jp>
2018-07-01 13:59:17 +0900

    プッシュテスト

--- app/src/main/java/com/example/enpit_p12/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/enpit_p12/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     //テスト
+    //test
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
